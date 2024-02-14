0d0d459f752e198eb2fbf33aa80a4997d8fababa
Ajeya Bhat <ajeya@outpost.run>
2024-02-14 14:31:22 +0530

    updated version

--- outpostcli/constants.py
+++ outpostcli/constants.py
@@ -1,1 +1,1 @@
-cli_version = "0.0.6"
+cli_version = "0.0.7"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="outpostcli",
-    version="0.0.6",
+    version="0.0.7",
     py_modules=["outpostcli"],
     install_requires=["Click", "outpostkit"],
     entry_points={
